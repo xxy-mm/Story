ca3b8af793e9674519c40fdb487b466311229198
darian <xxy.moming@gmail.com>
2024-03-28 19:41:51 +0800

    Initial Commit

---
+++ Rele2/Rele2App.swift
@@ -0,0 +1,17 @@
+//
+//  Rele2App.swift
+//  Rele2
+//
+//  Created by Darian Mitchell  on 2024/3/28.
+//
+
+import SwiftUI
+
+@main
+struct Rele2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
